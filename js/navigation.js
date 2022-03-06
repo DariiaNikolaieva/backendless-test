0d863821aa4f3771b9a065a9f8506719911626aa
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2022-03-06 21:13:09 +0200

    feat: add func of changing nav clickers

---
+++ js/navigation.js
@@ -0,0 +1,24 @@
+const navForm = document.querySelector("header form");
+
+const changeNavType = function () {
+  if (window.matchMedia("(min-width: 1080px)").matches) {
+    navForm.innerHTML = `<label><input type="radio" name="theme" value="light-theme" checked/>Light Theme</label>
+    <label><input type="radio" name="theme" value="dark-theme" />Dark Theme</label>
+    <label><input type="radio" name="theme" value="custom-theme" />Grey Theme</label>`;
+  } else if (
+    window.matchMedia("(min-width: 641px) and (max-width: 1079px)").matches
+  ) {
+    navForm.innerHTML = `<button value="light-theme">Light theme</button>
+  <button value="dark-theme">Dark theme</button>
+  <button value="custom-theme">Backendless theme</button>`;
+  } else if (window.matchMedia("(max-width: 640px)").matches) {
+    navForm.innerHTML = `<select name="theme">
+    <option value="light-theme" selected>Light theme</option>
+    <option value="dark-theme">Dark theme</option>
+    <option value="custom-theme">Backendless theme</option>
+  </select>`
+  }
+};
+
+window.addEventListener("load", changeNavType);
+window.addEventListener("resize", changeNavType);
